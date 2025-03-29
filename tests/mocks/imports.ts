5c17b3037510f9b723ea9eb9b0a9172f020b69b1
seveibar <seveibar@gmail.com>
2025-03-29 12:28:17 -0700

    remove useless fixtures

--- tests/mocks/imports.ts
+++
@@ -1,8 +0,0 @@
-// Mock external imports
-export const Key = ({ name }: { name: string }) => {
-  return { type: "Key", props: { name } };
-};
-
-export const A_1N4148WS = (props: any) => {
-  return { type: "A_1N4148WS", props };
-};
